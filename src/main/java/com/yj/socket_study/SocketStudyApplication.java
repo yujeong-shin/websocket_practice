df3aa3025c62413c85d531f25f8062a77c4a5647
yujeong-shin <vouobb@naver.com>
2024-02-17 23:03:55 +0900

    first commit

---
+++ src/main/java/com/yj/socket_study/SocketStudyApplication.java
@@ -0,0 +1,13 @@
+package com.yj.socket_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yj/socket_study/SocketStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.socket_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
